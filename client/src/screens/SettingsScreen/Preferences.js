1dc331d48332567e8000837d924f679c956cbfb3
Eric Jiang Qian <eric@192-168-1-103.tpgi.com.au>
2020-11-12 17:36:45 +1100

    added basic swipe screen

--- client/src/screens/SettingsScreen/Preferences.js
+++ client/src/screens/SettingsScreen/Preferences.js
@@ -1,21 +1,23 @@
 
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { createStackNavigator,createAppContainer } from 'react-navigation';
-//import ProfileScreen from './Profile';
-//import PreferencesScreen from './Preferences';
-
+import { View, Text, Button} from 'react-native';
 
 export default class PreferencesScreen extends Component {
+  saveChanges = () => {
+    // do a post
+   this.props.navigation.pop();
+ };
+
   render() {
-  return (<View>
-            <Text> Preferences </Text>
-            <Text> Interested in </Text>
-            <Text> Location </Text>
-            <Text> Max Age </Text>
-            <Text> Min Age </Text>
-            <Text> Maximum Distance </Text>
-          </View>
-  );
-}
+    return (<View>
+              <Text> Preferences </Text>
+              <Text> Interested in </Text>
+              <Text> Location </Text>
+              <Text> Max Age </Text>
+              <Text> Min Age </Text>
+              <Text> Maximum Distance </Text>
+              <Button title="Save" onPress={this.saveChanges} />
+            </View>
+        );
+    }
 }
