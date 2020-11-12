c3c1e91e58481d434ba372d0e584de5191207de5
Sheril Jose Sao <sheriljosesao@gmail.com>
2020-11-12 19:01:28 +1100

    Updated requirements.txt

--- backend/server/app.py
+++ backend/server/app.py
@@ -1,66 +1,4 @@
-# from flask import Flask, render_template, session
-# from flask_socketio import SocketIO, send, emit, join_room, leave_room
-#
-#
-# app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'secret!'
-# socketio = SocketIO(app)
-#
-# # @app.route('/login')
-# # def login():
-# #     check password - assuming thats fine
-# #     session['user'] = database.get("username")
-#
-# @app.route('/')
-# def startup():
-#     return """
-#      <!DOCTYPE html>
-#     <script src="//cdnjs.cloudflare.com/ajax/libs/socket.io/2.2.0/socket.io.js" integrity="sha256-yr4fRk/GU1ehYJPAs8P4JlTgu0Hdsp4ZKrx8bDEDC3I=" crossorigin="anonymous"></script>
-# <script type="text/javascript" charset="utf-8">
-#     var socket = io();
-#     socket.on('connect', function() {
-#         socket.emit('join', {username: 'You\\'re fucked''});
-#     });
-# </script>
-#     """
-#
-# # @app.route('/logout')
-# # def logout():
-# #     session.clear()
-#
-# @socketio.on('connect')
-# def handle_message():
-#     print('someone connected')
-#
-# @socketio.on('message')
-# def handle_message(message):
-#     room = session['room']
-#     send(message, room=room)
-#
-# @socketio.on('join')
-# def on_join(data):
-#     # username = session['user'].get('username')
-#     # room = username + data['other']
-#     username = data['username']
-#     room = data['room']
-#     join_room(room)
-#     session['room'] = room
-#     send('Oi dickhead, join this room ' + room)
-#     send(username + ' has entered the room.', room=room)
-#     send('blah', room='qwer')
-#     emit('message', 'blah', room='qwer')
-#
-# @socketio.on('leave')
-# def on_leave(data):
-#     # username = session['user'].get('username')
-#     # room = username + data['other']
-#     username = data['username']
-#     room = data['room']
-#     leave_room(room)
-#     send(username + ' has left the room.', room=room)
-#
-# if __name__ == '__main__':
-#     socketio.run(app)
+
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.getcwd() + '/../../'))
@@ -74,36 +12,10 @@ from backend.Classes.recommendation_system import Recommendation_System, Right_S
 from backend.Classes.message_system import Message_System
 from datetime import datetime, date
 
-
 from backend.server.models import *
 from backend.Classes.deals import Deals_system
 from backend.Classes.reservations import Reservation_system
 
-# from flask import Flask
-# from flask_bootstrap import Bootstrap
-# from flask_sqlalchemy import SQLAlchemy
-# from flask_login import LoginManager
-# from flask_jsglue import JSGlue
-# from config import Config
-#
-# # Extension Initialisation
-# bootstrap = Bootstrap()
-# db = SQLAlchemy()
-# login_manager = LoginManager()
-# login_manager.login_view = 'auth.login'
-# jsglue = JSGlue()
-#
-# # Factory Function
-# def create_app():
-#     # Create Flask Instance
-#     app = Flask(__name__, static_url_path='/static')
-#     app.config.from_object(Config)
-#
-#     # Apply Extension to Flask Instance
-#     bootstrap.init_app(app)
-#     db.init_app(app)
-#     login_manager.init_app(app)
-#     jsglue.init_app(app)
 
 app = Flask(__name__, template_folder='../templates')
 app.config['SECRET_KEY'] = 'user_side#'
