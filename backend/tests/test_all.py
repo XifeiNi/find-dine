cac5271b4a52bbdbfcae23b4ee55c4a7247920fe
AshAbay <arsha.abayasingam99@gmail.com>
2020-11-13 18:38:12 +1100

    Test program

--- backend/tests/test_all.py
+++ backend/tests/test_all.py
@@ -12,51 +12,75 @@ from backend.server.simulation import app, signup, login, current_user, logout
 
 
 class TestSignup(unittest.TestCase):
-    with app.app_context():
-        print("Testing authentication:")
-        command = ""
-        while True:
-            command = input("commands: signup, signup dummy, login, logout, exit: ")
-            if command == "exit":
-                break
+    def command_centre(self):
+        with app.app_context():
+            print("Testing authentication:")
+            command = ""
+            while True:
+                command = input("commands: signup, signup dummy, login, logout, recommendations, exit: ")
+                if command == "exit":
+                    break
 
-            if command == "get user":
-                print(current_user.get_cu())
-                continue
+                if command == "get user":
+                    print(current_user.get_cu())
+                    continue
 
-            if command == "signup":
-                request = {'email': input("enter email: "), 'username': input("enter username: "),
-                           'f_name': input("enter first name: "), 'l_name': input("enter last name: "),
-                           'password': input("enter password: "), 'password_repeat': input("confirm password: "),
-                           'dob': input("enter DOB (YYYY-MM-DD): "), 'min_target': input("enter min matching age: "),
-                           'max_target': input("enter max matching age: "),
-                           'gender': input("enter gender (male, female, other): "),
-                           'gender_preference': input("enter gender preference (male, female, everyone): "),
-                           'bio': input("enter bio: "), 'location': input("enter matching location: "),
-                           'max_match_distance': input("enter max match distance: ")}
+                if command == "signup":
+                    request = {'email': input("enter email: "), 'username': input("enter username: "),
+                               'f_name': input("enter first name: "), 'l_name': input("enter last name: "),
+                               'password': input("enter password: "), 'password_repeat': input("confirm password: "),
+                               'dob': input("enter DOB (YYYY-MM-DD): "), 'min_target': input("enter min matching age: "),
+                               'max_target': input("enter max matching age: "),
+                               'gender': input("enter gender (male, female, other): "),
+                               'gender_preference': input("enter gender preference (male, female, everyone): "),
+                               'bio': input("enter bio: "), 'location': input("enter matching location: "),
+                               'max_match_distance': input("enter max match distance: ")}
 
-                signup(request)
-                continue
+                    signup(request)
+                    continue
 
-            if command == "signup dummy":
-                request = {'email': 'sascha.graham@gmail.com', 'username': 'sascha', 'f_name': 'sascha',
-                           'l_name': 'graham', 'password': 'awd', 'password_repeat': 'awd', 'dob': '1999-11-02',
-                           'min_target': '16', 'max_target': '22', 'gender': 'male', 'gender_preference': 'everyone',
-                           'bio': 'awd', 'location': '23 Cameron Ave Artarmon', 'max_match_distance': '200'}
+                if command == "signup dummy":
+                    request = {'email': 'sascha.graham@gmail.com', 'username': 'sascha', 'f_name': 'sascha',
+                               'l_name': 'graham', 'password': 'awd', 'password_repeat': 'awd', 'dob': '1999-11-02',
+                               'min_target': '16', 'max_target': '22', 'gender': 'male', 'gender_preference': 'everyone',
+                               'bio': 'awd', 'location': '23 Cameron Ave Artarmon', 'max_match_distance': '200'}
 
-                signup(request)
-                continue
+                    signup(request)
+                    continue
 
-            if command == "login":
-                request = {'username': input("enter username: "), 'password': input("enter password: ")}
+                if command == "login":
+                    request = {'username': input("enter username: "), 'password': input("enter password: ")}
 
-                login(request)
-                continue
+                    login(request)
+                    continue
 
-            if command == "logout":
-                logout()
-                continue
-
+                if command == "logout":
+                    logout()
+                    continue
+                if command == "recommendations":
+                    self.recommendations()
+    def recommendations(self):
+        # origin = "Main Library, University of New South Wales, Sydney, Australia"
+        origin = input("Your current locaton please: ")
+        recs_sys = Recommendation_System()
+        # current_user_id = current_user.id
+        # current_user_id = current_user.id
+        curr_user = current_user.get_cu()
+        if curr_user is None:
+            print ("Something is wrong. Someone must be logged in")
+            exit(101)
+        current_user_id = curr_user.id
+        user = User_Profile.query.filter_by(id=current_user_id).first()
+        user.location = origin
+        db.session.commit()
+        recommendations = recs_sys.getRecommendations(origin, current_user_id)
+        print(len(recommendations))
+        # To print during pytest, uncomment False Assertion
+        for recommendation in recommendations:
+            # event.user_id = owner.id
+            print("########################")
+            print("Username: ", recommendation['match_user_username'])
+            print("Distance: ", recommendation['distance'])
 
 class TestAll(unittest.TestCase):
 
